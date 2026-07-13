509417dade71dde968545065b517f7252ba9dcd6
ijasahamedstr <116961653+ijasahamedstr@users.noreply.github.com>
2026-07-13 12:06:43 +0530

    commit

--- Backend/config/env.js
+++ Backend/config/env.js
@@ -26,8 +26,8 @@ export const config = {
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
-    "https://cyberx7-cargo.onrender.com",
-    "https://cyberx7-cargo-nine.vercel.app",
+    "https://ceylon-cargo-server.vercel.app",
+    "https://www.ceyloncargo-int.com",
   ]),
 };
 
